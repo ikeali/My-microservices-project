9b5da7a0dc1ccee021e218aa2bab2da31af73d37
ikeali <ikechukwualisigwe01@gmail.com>
2026-05-05 07:46:11 -0700

    router update

--- product-service/products/urls.py
+++ product-service/products/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from products.views import ProductViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductViewSet)
+# router.register(r'products', ProductViewSet)
+router.register(r'', ProductViewSet)
 
 urlpatterns = router.urls
