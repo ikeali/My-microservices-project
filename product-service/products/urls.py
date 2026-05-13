892e12464c6827cc576b5e7c37d7a6c6d417bf1c
ikeali <ikechukwualisigwe01@gmail.com>
2026-05-13 15:43:13 -0700

    updated product urls

--- product-service/products/urls.py
+++ product-service/products/urls.py
@@ -1,10 +1,16 @@
 # urls.py
 
-from rest_framework.routers import DefaultRouter
-from products.views import ProductViewSet
+# from rest_framework.routers import DefaultRouter
+from django.urls import path
+from .views import ProductView
 
-router = DefaultRouter()
-router.register(r'products', ProductViewSet)
+# router = DefaultRouter()
+# # router.register(r'products', ProductViewSet)
 # router.register(r'', ProductViewSet)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+
+urlpatterns = [
+    path("create_product/", ProductView.as_view(), name="create_product")
+]
--- product-service/products/views.py
+++ product-service/products/views.py
@@ -1,9 +1,16 @@
-from rest_framework.viewsets import ModelViewSet
+# from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-class ProductViewSet(ModelViewSet):
+# class ProductViewSet(ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     permission_classes = [IsAuthenticated]
+
+
+class ProductView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
